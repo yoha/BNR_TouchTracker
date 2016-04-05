49c7b87db2aa8b49b69cbe43c509b9fa4acbd1ba
Yohannes Wijaya <readyforservice@icloud.com>
2016-04-05 11:32:13 +0700

    17. Touch Events and UIResponder: Drawing w/ DrawView

--- BNR_TouchTracker/DrawView.swift
+++ BNR_TouchTracker/DrawView.swift
@@ -9,6 +9,36 @@
 import UIKit
 
 class DrawView: UIView {
+    
+    // MARK: - Stored Properties
+    
     var currentLine: Line?
     var finishedLines = Array<Line>()
+    
+    // MARK: - Local Methods
+    
+    func strokeLine(line: Line) {
+        let bezierPath = UIBezierPath()
+        bezierPath.lineWidth = 10
+        bezierPath.lineCapStyle = CGLineCap.Round
+        
+        bezierPath.moveToPoint(line.begin)
+        bezierPath.addLineToPoint(line.end)
+        bezierPath.stroke()
+    }
+    
+    // MARK: - UIView Methods
+    
+    override func drawRect(rect: CGRect) {
+        // Draw finished lines in black
+        UIColor.blackColor().setStroke()
+        for line in self.finishedLines {
+            self.strokeLine(line)
+        }
+        if let line = self.currentLine {
+            // If there is a line currently being drawn, do it in red
+            UIColor.redColor().setStroke()
+            self.strokeLine(line)
+        }
+    }
 }
