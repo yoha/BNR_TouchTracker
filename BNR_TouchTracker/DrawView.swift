fe43d2eca75d5dcbc7c2b787448b6ad17436d790
Yohannes Wijaya <readyforservice@icloud.com>
2016-04-05 11:10:56 +0700

    17. Touch Events and UIResponder: Creating the Line Struct & DrawView

---
+++ BNR_TouchTracker/DrawView.swift
@@ -0,0 +1,14 @@
+//
+//  DrawView.swift
+//  BNR_TouchTracker
+//
+//  Created by Yohannes Wijaya on 4/5/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    var currentLine: Line?
+    var finishedLines = Array<Line>()
+}
---
+++ BNR_TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  BNR_TouchTracker
+//
+//  Created by Yohannes Wijaya on 4/5/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
