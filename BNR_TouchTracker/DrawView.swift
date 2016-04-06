90db01c6f32a1d2dfd42b9b248f4bc3119d3eb09
Yohannes Wijaya <readyforservice@icloud.com>
2016-04-06 14:44:41 +0700

    18. UIGestureRecognizer and UIMenuController: Multiple Gesture Recognizers

--- BNR_TouchTracker/DrawView.swift
+++ BNR_TouchTracker/DrawView.swift
@@ -14,6 +14,7 @@ class DrawView: UIView {
     
     var currentLines = [NSValue: Line]()
     var finishedLines = Array<Line>()
+    var selectedLineIndex: Int?
     
     // MARK: - IBInspectable properties
     
@@ -24,6 +25,12 @@ class DrawView: UIView {
     }
     
     @IBInspectable var currentLineColor: UIColor = UIColor.redColor() {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var selectedLineColor: UIColor = UIColor.greenColor() {
         didSet {
             self.setNeedsDisplay()
         }
@@ -50,9 +57,39 @@ class DrawView: UIView {
     func doubleTap(gestureRecognizer: UIGestureRecognizer) {
         print("Gesture recognized as a double tap")
         
+        self.selectedLineIndex = nil
         self.currentLines.removeAll(keepCapacity: false)
         self.finishedLines.removeAll(keepCapacity: false)
         self.setNeedsDisplay()
+    }
+    
+    func singleTap(gestureRecognizer: UIGestureRecognizer) {
+        print("Gesure recognized as a single tap")
+        
+        let tapLocationPoint = gestureRecognizer.locationInView(self)
+        self.selectedLineIndex = self.indexOfLineAtPoint(tapLocationPoint)
+        self.setNeedsDisplay()
+    }
+    
+    func indexOfLineAtPoint(point: CGPoint) -> Int? {
+        // Find a line close to point
+        for (index, line) in self.finishedLines.enumerate() {
+            let begin = line.begin
+            let end = line.end
+            
+            // Check a few points on the line
+            for t in CGFloat(0).stride(to: 1.0, by: 0.05) {
+                let x = begin.x + ((end.x - begin.x) * t)
+                let y = begin.y + ((end.y - begin.y) * t)
+                
+                // If tapped point is within 20 points, let's return this line
+                if hypot(x - point.x, y - point.y) < 20.0 { return index }
+            }
+            
+        }
+        
+        // If nothing is close enough to the tapped point, then we did not select a line
+        return nil
     }
     
     // MARK: NSCoder Methods
@@ -64,6 +101,11 @@ class DrawView: UIView {
         doubleTapGestureRecognizer.numberOfTapsRequired = 2
         doubleTapGestureRecognizer.delaysTouchesBegan = true
         self.addGestureRecognizer(doubleTapGestureRecognizer)
+        
+        let singleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: "singleTap:")
+        singleTapGestureRecognizer.delaysTouchesBegan = true
+        singleTapGestureRecognizer.requireGestureRecognizerToFail(doubleTapGestureRecognizer)
+        self.addGestureRecognizer(singleTapGestureRecognizer)
     }
     
     // MARK: - UIView Methods
@@ -76,6 +118,11 @@ class DrawView: UIView {
         self.currentLineColor.setStroke()
         for (_, line) in self.currentLines {
             self.strokeLine(line)
+        }
+        if let validIndex = self.selectedLineIndex {
+            self.selectedLineColor.setStroke()
+            let selectedLine = self.finishedLines[validIndex]
+            self.strokeLine(selectedLine)
         }
     }
     
